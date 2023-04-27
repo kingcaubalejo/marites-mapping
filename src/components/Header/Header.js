c3c951dd81e6b935bda906b9f9e82227fb838b6b
KCaubal1 <King.Caubalejo@t-mobile.com>
2023-04-27 13:26:46 +0800

    Typing and Header changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 import Container from "@components/Container";
 
@@ -9,17 +9,25 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <Container className={styles.headerContainer}>
+        <img src="" className={styles.headerLogo} />
         <p className={styles.headerTitle}>
           <Link href="/">Marites Locator - Alpha v1.0</Link>
         </p>
         <ul className={styles.headerLinks}>
           <li>
-            <a
+            {/* <a
               href="https://github.com/colbyfayock/next-leaflet-starter"
               rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
+            > */}
+            <FaGithub />
+            {/* </a> */}
+          </li>
+
+          <li>
+            <FaLinkedinIn />
+          </li>
+          <li>
+            <FaInstagram />
           </li>
         </ul>
       </Container>
